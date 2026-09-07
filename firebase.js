d93bcb1b66b8a4f337d50ceabab5ae6d6b5c3c8d
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-09-07 10:03:50 -0400

    feat: expose Firebase auth firestore and storage

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSpkvRKMXCXVhfcx7HYbV4IQsqeLvWOvQ",
@@ -10,3 +13,6 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
