6d4d6937b0f440dead08a58fd5afd458e5a06ec4
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-09-07 10:00:49 -0400

    feat: initialize Firebase web app

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSpkvRKMXCXVhfcx7HYbV4IQsqeLvWOvQ",
+  authDomain: "calmloop.firebaseapp.com",
+  projectId: "calmloop",
+  storageBucket: "calmloop.firebasestorage.app",
+  messagingSenderId: "988412283883",
+  appId: "1:988412283883:web:a26e4bf4e2778f48de67ae"
+};
+
+export const app = initializeApp(firebaseConfig);
