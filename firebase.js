2dc2278b736886789fabea13b49e74578b8dc11d
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-09-07 10:08:27 -0400

    refactor: remove Firebase Storage dependency

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSpkvRKMXCXVhfcx7HYbV4IQsqeLvWOvQ",
@@ -15,4 +14,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
